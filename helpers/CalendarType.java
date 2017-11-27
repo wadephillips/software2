88a48793e934808d4a6d2ec098aa6c795cb3e9ed
Wade Phillips <phillips.acupuncture@gmail.com>
2017-11-27 05:34:29 -0700

    Document calendar.helpers.*

--- helpers/CalendarType.java
+++ helpers/CalendarType.java
@@ -1,5 +1,8 @@
 package calendar.helpers;
 
+/**
+ * An enum for specifying a specific version of the calendar
+ */
 public enum CalendarType {
     MONTHLY, WEEKLY
 }
--- helpers/KeyValuePair.java
+++ helpers/KeyValuePair.java
@@ -1,21 +1,41 @@
 package calendar.helpers;
 
+/**
+ * A helper class for building an object that contains a numeric id as the key and a String as the value
+ */
 public class KeyValuePair {
+
+    /**
+     * The object's id
+     */
     private final long key;
 
+    /**
+     * The object's String Value
+     */
     private final String value;
 
+    /**
+     * The constructor
+     * @param key the id
+     * @param value a string
+     */
     public KeyValuePair(long key, String value) {
         this.key = key;
         this.value = value;
     }
 
+    /**
+     * Returns the object's id
+     *
+     * @return the id
+     */
     public long getKey() {
         return key;
     }
 
     /**
-     *
+     * Gets the String value of the object
      */
     @Override
     public String toString() {
